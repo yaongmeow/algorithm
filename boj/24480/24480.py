9b36f2f72103e315f99d7cd2ffa97ac1ebb0be2d
yaongmeow <minnie_00@naver.com>
2025-05-25 20:15:18 +0900

    boj-24480-python

---
+++ boj/24480/24480.py
@@ -0,0 +1,31 @@
+from collections import deque
+import sys
+
+n, m, r = map(int, sys.stdin.readline().strip().split())
+arr = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    a, b = map(int, sys.stdin.readline().strip().split())
+    arr[a].append(b)
+    arr[b].append(a)
+
+vis = [False] * (n + 1)
+cnt = [0] * (n + 1)
+turn = 1
+
+st = deque()
+st.append(r)
+
+while len(st) > 0:
+    pos = st.pop()
+    if vis[pos]: continue
+    vis[pos] = True
+    cnt[pos] = turn
+    turn+=1
+    arr[pos].sort()
+    for i in arr[pos]:
+        if vis[i] == False:
+            st.append(i)
+
+for i in range(1, n+1):
+    print(cnt[i])
