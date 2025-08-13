e583eab22c41686f37283c1b80ac39fb12d2e9c8
minnie_00 <minnie_00@naver.com>
2025-08-13 11:31:25 +0900

    fix: 파일 서식 수정

--- swea/2072/Solution.java
+++ swea/2072/Solution.java
@@ -6,19 +6,19 @@ public class Solution {
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
- 
+
         for(int test_case = 1; test_case <= T; test_case++)
         {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int answer = 0;
             for(int i = 0; i < 10; i++){
-                 int tmp = Integer.parseInt(st.nextToken());
+                int tmp = Integer.parseInt(st.nextToken());
                 if(tmp%2 == 0) continue;
                 answer += tmp;
             }
             System.out.print("#" + Integer.toString(test_case) + " ");
             System.out.println(answer);
- 
+
         }
     }
 }
