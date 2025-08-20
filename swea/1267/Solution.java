23cd3a9cdeaf41f6675de44c166c610079f29dec
minnie_00 <minnie_00@naver.com>
2025-08-21 08:51:36 +0900

    swea-1267-java

---
+++ swea/1267/Solution.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+class Main{
+    static int V, E;
+    public static void main(String[] args) throws Exception{
+        System.setIn(new FileInputStream("sample_input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder("");
+        int T = 10;
+        for(int tc = 1; tc <= T; tc++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            V = Integer.parseInt(st.nextToken());
+            E = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+
+            // int[][] a = new int[E][2];
+            int[] indegree = new int [V + 1];
+            List<Integer>[] g = new List[V + 1]; for(int i = 1; i <= V; i++) g[i] = new ArrayList<>();
+            for(int i = 0; i < E; i++){
+                // a[i][0] = Integer.parseInt(st.nextToken()); // from
+                // a[i][1] = Integer.parseInt(st.nextToken()); // to
+                int from = Integer.parseInt(st.nextToken());
+                int to = Integer.parseInt(st.nextToken());
+                indegree[to] += 1;
+                g[from].add(to);
+            }
+
+            Queue<Integer> q = new ArrayDeque<>();
+            boolean[] v = new boolean[V + 1];
+            for(int i = 1; i <= V; i++){
+                if(indegree[i] == 0){
+                    q.offer(i);
+                    v[i] = true;
+                }
+            }
+
+            int[] answer = new int [V];
+            int idx = 0;
+
+            while(!q.isEmpty()){
+                int now = q.poll();
+                answer[idx++] = now;
+                for(Integer p: g[now]){
+                    if(v[p]) continue;
+                    indegree[p]--;
+                    if(indegree[p] == 0){
+                        v[p] = true;
+                        q.offer(p);
+                    }
+                }
+            }
+
+            sb.append("#" + tc + " ");
+            for(int n : answer) sb.append(n + " ");
+            sb.append("\n");
+        }
+        System.out.print(sb.toString());
+    }
+}
